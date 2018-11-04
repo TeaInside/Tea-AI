ddb711eec6188fbb7abd694d98bf4f3f2333c393
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-04 20:42:26 +0700

    Prepare build file

--- build.php
+++ build.php
@@ -1,11 +1,32 @@
 <?php
 
-if (!file_exists("/tmp/phpcpp")) {
-	$commands = [
-		"wget https://api.github.com/repos/CopernicaMarketingSoftware/PHP-CPP/tarball/v2.1.2 -O /tmp/phpcpp",
-		"cd /tmp && tar -xf phpcpp && cd *PHP* && make -j 5 && sudo make install -j 5",
+
+$descriptorspec = [
+   0 => ["pipe", "w"], // STDIN
+   1 => ["file", "php://stdout", "w"], // STDOUT
+   2 => ["file", "php://stderr", "w"]  // STDERR
+];
+
+$commands = [
+	[
+		(!file_exists("/tmp/phpcpp")),
+		"wget https://api.github.com/repos/CopernicaMarketingSoftware/PHP-CPP/tarball/v2.1.2 -O /tmp/phpcpp"
+	],
+	[
+		true,
+		"cd /tmp && tar -xf phpcpp && cd *PHP* && make -j 5 && sudo make install -j 5"
+	],
+	[
+		true,
 		"cd ~/repo && composer install -n --prefer-dist -vvv"
-	];
-	$proc = proc_open($cmd, $desc, $pipes);
-	
+	],
+];
+
+
+foreach($commands as $command) {
+	if ($command[0]) {
+		$proc = proc_open($command[1], $descriptorspec, $pipes);
+		fclose($pipes[0]);
+		proc_close($proc);
+	}
 }
